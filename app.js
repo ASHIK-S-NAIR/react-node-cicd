80e592987b8b5cf188d7295e5fb6f79f62f0f5cd
Ashik S Nair <aashiq5342@gmail.com>
2022-07-03 14:54:40 +0530

    configure github actions try three

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get("/name", (req, res) => {
-  res.send("Ashik");
+  res.send("Ashik S Nair");
 });
 
 app.listen(5000, () => console.log("Server is runnning on port 5000"));
